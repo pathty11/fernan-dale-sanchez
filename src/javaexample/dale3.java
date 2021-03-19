c69b9a0ad82752ac4cdb2cb1d50f0c20282038d1
Stmaderz1 <Tmaderz48@gmail.com>
2021-03-19 13:06:57 +0800

    commit

---
+++ src/javaexample/dale3.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample;
+
+/**
+ *
+ * @author stmaderz
+ */
+
+    
+    import java.util.Scanner;
+public class dale3 {
+ 
+
+
+    public static void main(String[] args) 
+    {
+        int count;
+        String temp;
+        Scanner scan = new Scanner(System.in);
+        
+        //User will be asked to enter the count of strings 
+        System.out.print("Enter number of strings you would like to enter:");
+        count = scan.nextInt();
+        
+        
+        String str[] = new String[count];
+        Scanner scan2 = new Scanner(System.in);
+        
+        //User is entering the strings and they are stored in an array
+        System.out.println("Enter the Strings one by one:");
+        for(int i = 0; i < count; i++)
+        {
+            str[i] = scan2.nextLine();
+        }
+        scan.close();
+        scan2.close();
+        
+        //Sorting the strings
+        for (int i = 0; i < count; i++) 
+        {
+            for (int j = i + 1; j < count; j++) { 
+                if (str[i].compareTo(str[j])>0) 
+                {
+                    temp = str[i];
+                    str[i] = str[j];
+                    str[j] = temp;
+                }
+            }
+        }
+        
+        
+    }
+}
+
+
+
