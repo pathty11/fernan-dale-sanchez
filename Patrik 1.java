ebbf2edd160aa14a4b56e614cf6be6b5489d47b9
pathty11 <79315931+pathty11@users.noreply.github.com>
2021-03-20 14:46:57 +0800

    Add files via upload

---
+++ Patrik 1.java
@@ -0,0 +1,24 @@
+
+package samgy;
+
+public class Samgy {
+
+    public static void main(String[] args) {
+
+        System.out.println("Good day! Welcome to OLGA'S SAMGYUPSAL");
+        System.out.println("200 pesos per head");
+        System.out.println("--------------------------------------------");
+        System.out.println("THE NEW NORMAL!");
+        System.out.println("NO Face Mask No Entry");
+        System.out.println("1.5 Meter Table Distance");
+        System.out.println("50% of our total Seating Capacity");
+        System.out.println("Frequent Store Sanitazion");
+        System.out.println("Temperature Check");
+        System.out.println("Disenfectant Mat on Enterance");
+        
+        System.out.println("--------------------------------------------");
+        reserve com= new reserve();
+        com.menu();
+    }
+    
+}
